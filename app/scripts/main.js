387d851e9a056be9efed2afd1d594cba05ae8c82
Tamas Erdos <tamas@tamaserdos.com>
2014-08-27 18:03:41 -0400

    v1.0

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,26 @@
+'use strict';
 console.log('\'Allo \'Allo!');
+
+var convert = function() {
+    var input = $('#indata').val();
+    var nry = [];
+    _(input.split('\n')).each(function(el) {
+        nry.push(el.split('\t'));
+    });
+    var titles = _(nry).first();
+    var data = _(nry).rest();
+    var objects = [];
+    _(data).map(function(item) {
+        var obj = {};
+        _(titles).each(function(title, i) {
+            obj[title] = item[i];
+        });
+        objects.push(obj);
+    });
+    $('#output').text(JSON.stringify(objects));
+};
+$(function() {
+    $('button').click(function() {
+        convert();
+    });
+});
